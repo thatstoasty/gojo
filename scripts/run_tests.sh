3204a483aa3d3fba72dfc3e6f6691515291d45e9
Mikhail Tavarez <miktavarez@gmail.com>
2024-08-14 22:37:36 -0500

    add scripts

---
+++ scripts/run_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p tmp
+
+echo -e "Building gojo package and copying tests."
+./scripts/build.sh package
+mv gojo.mojopkg tmp/
+cp -R tests/ tmp/tests/
+
+echo -e "\nBuilding binaries for all examples."
+pytest tmp/tests
+
+echo -e "Cleaning up the test directory."
+rm -R tmp
