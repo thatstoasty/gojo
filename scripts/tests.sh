33a2f634527157813924782a0c7db6261d242095
Mikhail Tavarez <miktavarez@gmail.com>
2024-09-06 14:29:27 -0500

    remove build script

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -5,10 +5,10 @@ mkdir -p $TEMP_DIR
 
 echo "[INFO] Building gojo package and copying tests."
 cp -R test/ $TEMP_DIR
-mojo package src/gojo -o $TEMP_DIR/gojo.mojopkg
+magic run mojo package src/gojo -o $TEMP_DIR/gojo.mojopkg
 
 echo "[INFO] Running tests..."
-mojo test $TEMP_DIR
+magic run mojo test $TEMP_DIR
 
 echo "[INFO] Cleaning up the test directory."
 rm -R $TEMP_DIR
