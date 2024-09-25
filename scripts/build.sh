88752a72f5cb029e678dab82301c4d583e9a1783
Mikhail Tavarez <miktavarez@gmail.com>
2024-09-25 08:25:29 -0500

    fix typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,12 +3,15 @@
 set -e
 
 # The environment to build the package for. Usually "default", but might be "nightly" or others.
-ENVRIONMENT="${1-default}"
-if [[ "${ENVRIONMENT}" == "--help" ]]; then
-    echo "Usage: ENVRIONMENT - Argument 1 corresponds with the environment you wish to build the package for."
+MAX_CHANNEL="https://conda.modular.com/max"
+ENV="${1-default}"
+if [[ "${ENV}" == "--help" ]]; then
+    echo "Usage: ENV - Argument 1 corresponds with the environment you wish to build the package for."
     exit 0
+else if [[ "${ENV}" == "nightly" ]]; then
+    MAX_CHANNEL="https://conda.modular.com/max-nightly"
 fi
 
-magic run template -m "${ENVRIONMENT}"
-rattler-build build -r src -c https://conda.modular.com/max -c conda-forge --skip-existing=all
+magic run template -m "${ENV}"
+rattler-build build -r src -c "${MAX_CHANNEL}" -c conda-forge --skip-existing=all
 rm src/recipe.yaml
